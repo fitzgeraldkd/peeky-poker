3531e1964650b508362ea339aae714d6c01534d0
onatoko <kfonatoko@gmail.com>
2024-06-08 20:19:57 -0400

    Add card count to deck

--- scenes/deck.gd
+++ scenes/deck.gd
@@ -13,8 +13,8 @@ func _process(_delta):
 	pass
 
 func reset():
-	for n in get_children():
-		remove_child(n)
+	for n in $Cards.get_children():
+		$Cards.remove_child(n)
 		n.queue_free()
 
 	for value in range(1, 14):
@@ -23,18 +23,19 @@ func reset():
 			var card = card_scene.instantiate()
 			card.value = value
 			card.suit = suit
-			add_child(card)
+			$Cards.add_child(card)
 
 func draw_card():
-	var cards = get_children()
+	var cards = $Cards.get_children()
 
-	if get_children().size() == 0:
+	if cards.size() == 0:
 		cards = discard_node.get_node("Cards").get_children()
 		for card in cards:
 			discard_node.get_node("Cards").remove_child(card)
 			card.set_face_up(false)
-			add_child(card)
+			$Cards.add_child(card)
 
 	var card = cards[randi() % cards.size()]
-	remove_child(card)
+	$Cards.remove_child(card)
+	$Label.text = str($Cards.get_children().size())
 	return card
